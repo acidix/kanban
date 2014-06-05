a1d836368341176452d5f6c27fa8c6559f4437dd
acidix <thomas.uhde@acidix.net>
2014-06-05 23:08:33 +0200

    Finished the dynamic UI & started using LocalStorage to save the current window state

--- public/app.js
+++ public/app.js
@@ -2,13 +2,93 @@ $( function() {
     $('#refresh').on('click', function (e) {
          window.location.href = '/refresh';
     })
-})
 
+    $( "#active-content" ).sortable();
+    $( "#active-content" ).disableSelection();
+    $( "#backburner-content" ).sortable();
+    $( "#backburner-content" ).disableSelection();
+    $( "#completed-content" ).sortable();
+    $( "#completed-content" ).disableSelection();
+
+    $('.persist-state').persistState();
+    $('#reset').click(function(){
+      $(':ow-persistState').persistState('clear');
+      window.location.href = '/';
+    });
+
+});
+
+
+function switchActiveClass(classes) {
+  $('#active').toggleClass(classes);
+  $('#active-content').toggleClass(classes);
+}
+
+function resizeActive(action) {
+    if (action=='enlarge') {
+      if ($('#active').hasClass('col-md-6')) {
+        switchActiveClass('col-md-6 col-md-8');
+      } else {
+        switchActiveClass('col-md-8 col-md-10');
+      }
+    } else {
+      if ($('#active').hasClass('col-md-10')) {
+        switchActiveClass('col-md-10 col-md-8');
+      } else {
+        switchActiveClass('col-md-8 col-md-6');
+      }
+    }
+}
 function toggleItems(key) {
   $('.' + key).toggle(400);
-  $('#btn-' + key).toggleClass('btn-inactive');
+  $('#btn-' + key).toggleClass('btn-inactive', 400);
 }
 function toggleHanging() {
   $('.hanging').toggle(400);
-  $('.toggle-hanging').toggleClass('glyphicon-plus-sign glyphicon-minus-sign')
+  $('.toggle-hanging').toggleClass('glyphicon-plus-sign glyphicon-minus-sign', 400)
 }
+function toggleSidebar(kind) {
+  if ($('#' + kind).hasClass('col-md-3')) {
+    $('#' + kind + '-content').children().css('visibility','hidden');
+    resizeActive('enlarge');
+  } else {
+    $('#' + kind + '-content').children().css('visibility','visible');
+    resizeActive('reduce');
+  }
+
+  $('#' + kind).children().first().toggle();
+  $('#' + kind).toggleClass('col-md-3 col-md-1');
+  $('#' + kind).children().last().toggleClass('glyphicon-indent-right glyphicon-indent-left');
+
+  $('#' + kind + '-content').toggleClass('col-md-3 col-md-1');
+}
+function expandViewport() {
+  $('#main').toggleClass('container container-fluid', 400);
+  $('#expandicon').toggleClass('glyphicon-resize-full glyphicon-resize-small', 400);
+}
+
+$.ow.persistState.elementPersistence['div'] = {
+    saveState: function($el) {
+        return { classes: $el.attr('class'),
+                 hidden: $el.is(':hidden') };
+    },
+    restoreState: function($el, state) {
+        if(!state) return;
+        $el.removeClass();
+        $el.addClass(state.classes);
+        if (state.hidden)
+          $el.css('visibility', 'hidden');
+    }
+};
+
+$.ow.persistState.elementPersistence['button'] = {
+    saveState: function($el) {
+        return { hidden: $el.hasClass('btn-inactive')};
+    },
+    restoreState: function($el, state) {
+        if(!state) return;
+        if ( state.hidden ) {
+          toggleItems($el.html())
+        }
+    }
+};
